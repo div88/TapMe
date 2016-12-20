136014a4a24bca5f9c051b4680dbb94d53a120d5
div88 <diviyengar@gmail.com>
2016-12-19 18:52:48 -0800

    Initial Commit

--- Tap Me/ViewController.m
+++ Tap Me/ViewController.m
@@ -19,11 +19,15 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(IBAction)buttonPressed {
+    NSLog(@"Pressed!");
+}
+
 
 
 @end
